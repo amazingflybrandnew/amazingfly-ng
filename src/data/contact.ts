8ede392e336327e6e6925d6366d85123196ea8a5
amazingflybrandnew <albert.afolabi44@gmail.com>
2026-08-24 14:05:23 +0100

    Add verified Amazingfly contact details

---
+++ src/data/contact.ts
@@ -0,0 +1,14 @@
+export const contactDetails = {
+  phoneDisplay: "08145291800",
+  phoneHref: "tel:+2348145291800",
+  whatsappDisplay: "08145291800",
+  whatsappHref: "https://wa.me/2348145291800",
+  email: "amazingflyinternational@gmail.com",
+  emailHref: "mailto:amazingflyinternational@gmail.com",
+  address: "Suite D25, Kaita Plaza, Gwagwalada, Abuja, Nigeria",
+  businessHours: "Open 24 hours daily",
+  facebook:
+    "https://www.facebook.com/share/1JNh1znHhL/?mibextid=wwXIfr",
+  instagram:
+    "https://www.instagram.com/amazingflyinternational?igsi=OXkwY3FkMTl4dXM4&utm_source=qr",
+} as const;
